fe051ce2e5db32415a861dbe9c2acf0dcd302824
Zac Lim Zheng Yang <zhengyanglim057@gmail.com>
2022-04-26 15:26:48 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -33,10 +33,8 @@ public class MyStringStackTest extends TestCase {
 	public void testPush() {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
-		assertTrue (stack.isEmpty());
 		stack.push (s1);
-        stack.push (s2);
-        assertFalse (stack.isEmpty());
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
